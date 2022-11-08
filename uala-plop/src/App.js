35c6ec86f506afa892912a1ede6ed12e1d1f32e7
Josep Valls <alfredvays@gmail.com>
2022-11-08 21:22:20 +0100

    contexto refactorizado en index

--- uala-plop/src/App.js
+++ uala-plop/src/App.js
@@ -2,80 +2,56 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import LoginPage from './components/auth/LoginPage.js';
 import './App.css';
 import Layout from './components/layout/Layout.js';
-import { useState } from 'react';
-import { logout } from './components/auth/service.js';
 import NewAdPage from './components/ads/NewAdPage.js';
 import AdPage from './components/ads/AdPage.js';
 import AdsPage from './components/ads/AdsPage.js';
 import RequireAuth from './components/auth/RequireAuth.js';
 import NotFound from './components/common/error/notFound/NotFound.js';
 import Main from './components/layout/Main.js';
-import { AuthContextProvider } from './components/auth/Context.js';
+import { useAuthContext } from './components/auth/Context.js';
 
-function App({ haveToken }) {
-  const [isLogged, setIsLogged] = useState(haveToken);
-
-  const titleApp = 'Uala Plop';
-
-  const handleLogin = () => setIsLogged(true);
-
-  const handleLogout = () => {
-    logout();
-    setIsLogged(false);
-  };
-
+function App() {
+  const { isLogged, handleLogin, titleApp } = useAuthContext();
 
   return (
     <div className='app'>
-      <AuthContextProvider value={{isLogged, handleLogin, handleLogout,titleApp}}>
-        <Routes>
+      <Routes>
+        <Route
+          path='/login'
+          element={
+            !isLogged ? (
+              <LoginPage titleApp={titleApp} onLogin={handleLogin} />
+            ) : (
+              <Navigate to='/ads' />
+            )
+          }
+        />
+        ;
+        <Route
+          path='/ads'
+          element={
+            <RequireAuth>
+              <Layout />
+            </RequireAuth>
+          }
+        >
+          <Route index element={<Main title='Home' children={<AdsPage />} />} />
           <Route
-            path='/login'
-            element={
-              !isLogged ? (
-                <LoginPage titleApp={titleApp} onLogin={handleLogin} />
-              ) : (
-                <Navigate to='/ads' />
-              )
-            }
+            path=':id'
+            element={<Main title='Advertisment Detail' children={<AdPage />} />}
           />
           ;
-          <Route
-            path='/ads'
-            element={
-              <RequireAuth>
-                <Layout/>               
-              </RequireAuth>
-            }
-          >
-            <Route
-              index
-              element={<Main title='Home' children={<AdsPage />} />}
-            />
-            <Route
-              path=':id'
-              element={
-                <Main title='Advertisment Detail' children={<AdPage />} />
-              }
-            />
-            ;
-            <Route path='new' element={<Main children={<NewAdPage />} />} />;
-          </Route>
-          <Route
-            path='/'
-            element={
-              isLogged ? <Navigate to='/ads' /> : <Navigate to='/login' />
-            }
-          />
-          ;
-          <Route
-            path='/404'
-            element={<NotFound error={{ message: '404' }} />}
-          />
-          ;
-          <Route path='*' element={<Navigate to='/404' />} />
-        </Routes>
-      </AuthContextProvider>
+          <Route path='new' element={<Main children={<NewAdPage />} />} />;
+        </Route>
+        <Route
+          path='/'
+          element={isLogged ? <Navigate to='/ads' /> : <Navigate to='/login' />}
+        />
+        ;
+        <Route path='/404' element={<NotFound error={{ message: '404' }} />} />
+        ;
+        <Route path='*' element={<Navigate to='/404' />} />
+      </Routes>
     </div>
   );
 }
--- uala-plop/src/components/auth/Context.js
+++ uala-plop/src/components/auth/Context.js
@@ -1,11 +1,33 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useState } from "react";
+import { logout } from './service.js';
 
 const AuthContext = createContext();
 
-export const AuthContextProvider = AuthContext.Provider;
 export const AuthContextConsumer = AuthContext.Consumer;
 
 AuthContext.displayName = 'App';
+
+export const AuthContextProvider = ({haveToken, children}) => {
+
+    const titleApp = 'Uala Plop';
+
+    const [isLogged, setIsLogged] = useState(haveToken);
+
+    
+  const handleLogin = () => setIsLogged(true);
+
+  const handleLogout = () => {
+    logout();
+    setIsLogged(false);
+  };
+
+  return (
+    <AuthContext.Provider value={{isLogged, handleLogin, handleLogout, titleApp}}>
+        {children}
+    </AuthContext.Provider>
+  )
+
+}
 
 export const useAuthContext = () =>{
     const value = useContext(AuthContext);
