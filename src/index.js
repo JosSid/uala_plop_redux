10671da656ff91f5a5fd4c77821515325b18f9c0
Josep Valls <alfredvays@gmail.com>
2023-01-02 17:23:00 +0100

    Creado componente Root en index para pasar store

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -7,6 +8,7 @@ import storage from './utils/storage.js';
 import { setAuthorizationHeader } from './api/client.js';
 import { AuthContextProvider } from './components/auth/Context.js';
 import configureStore from './store';
+import Root from './Root';
 
 const accessToken = storage.get('auth');
 setAuthorizationHeader(accessToken);
@@ -17,10 +19,10 @@ const store = configureStore({auth: !!accessToken});
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
+    <Root store={store}>
       <AuthContextProvider haveToken={!!accessToken}>
         <App />
       </AuthContextProvider>
-    </Router>
+    </Root>
   </React.StrictMode>
 );
