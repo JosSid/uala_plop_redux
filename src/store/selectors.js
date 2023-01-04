1bfd05115d2555c61a810794feaa8c3e2d760ef1
Josep Valls <alfredvays@gmail.com>
2023-01-04 19:35:38 +0100

    Selector getAdById en adPage

--- src/store/selectors.js
+++ src/store/selectors.js
@@ -1,5 +1,8 @@
-export const getIsLogged = state => state.auth;
+export const getIsLogged = (state) => state.auth;
 
-export const getListAds = state => state.ads;
+export const getListAds = (state) => state.ads;
 
-export const getListTags = state => state.tags;
+export const getListTags = (state) => state.tags;
+
+export const getAdById = (state, adId) =>
+  getListAds(state).find((ad) => ad.id === adId);
