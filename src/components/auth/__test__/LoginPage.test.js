5873194406363b163a66b7b3834a271b6ad2efe0
Josep Valls <alfredvays@gmail.com>
2023-01-13 23:05:15 +0100

    LoginPage test component

--- src/components/auth/__test__/LoginPage.test.js
+++ src/components/auth/__test__/LoginPage.test.js
@@ -1,20 +1,49 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import LoginPage from '../LoginPage.js';
-import { defaultState} from '../../../store/reducers.js'
+import { defaultState } from '../../../store/reducers.js';
+import { authLogin } from '../../../store/actions.js';
+import '@testing-library/jest-dom/extend-expect';
+
+jest.mock('../../../store/actions.js')
 
 describe('LoginPage', () => {
-  test('Snapshot', () => {
-        const store = {
-        getState: () => defaultState,
-        dispatch: () => {},
-        subscribe: () => {},
-    }
-    const {container} = render(
-      <Provider store= {store}>
+  const store = {
+    getState: () => defaultState,
+    dispatch: () => {},
+    subscribe: () => {},
+  };
+
+  const renderComponent = () => 
+    render(
+      <Provider store={store}>
         <LoginPage />
       </Provider>
     );
+  
+  test('Snapshot', () => {
+    const { container } = renderComponent();
     expect(container).toMatchSnapshot();
+  });
+  test('Should dispatch authLogin action', () => {
+
+    const email = 'pepe@pepe.com';
+    const password = '1234'
+
+    renderComponent();
+
+    const eMailInput = screen.getByLabelText(/eMail/);
+    const passwordInput = screen.getByLabelText(/password/);
+    const checkboxInput = screen.getByText('Check for recording Login');
+    const submitButton = screen.getByRole('button');
+
+    expect(submitButton).toBeDisabled();
+    fireEvent.change(eMailInput, {target: {value: email}});
+    fireEvent.change(passwordInput, {target: { value: password}});
+    fireEvent.change(checkboxInput, {target: {checked: true}});
+    expect(submitButton).toBeEnabled();
+
+    fireEvent.click(submitButton);
+    expect(authLogin).toHaveBeenCalledWith({email, password});
   });
 });
