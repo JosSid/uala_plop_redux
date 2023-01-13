c5c9001f9ee851f34031e1023b5476563e36e4a0
Josep Valls <alfredvays@gmail.com>
2023-01-13 18:18:34 +0100

    adsLoad action test

--- src/store/__test__/asyncActions.test.js
+++ src/store/__test__/asyncActions.test.js
@@ -1,4 +1,4 @@
-import { authLogin, authLogout, authLoginFailure, authLoginRequest, authLoginSucces } from '../actions.js'
+import { authLogin, authLoginFailure, authLoginRequest, authLoginSucces, adsLoad, adsLoadedRequest, adsLoadedSucces, adsLoadedFailure } from '../actions.js'
 
 describe('authLogin', () => {
     const credentials = 'credentials';
@@ -24,7 +24,7 @@ describe('authLogin', () => {
         test('Should follow the error flow', async () => {
             api.auth.login = jest.fn().mockRejectedValue(error);
             const promise = action(dispatch, undefined, {api, router});
-            await expect(promise).rejects.toThrow(error)
+            await expect(promise).rejects.toThrow(error);
             expect(dispatch).toHaveBeenNthCalledWith(1, authLoginRequest());
             expect(dispatch).toHaveBeenNthCalledWith(2, authLoginFailure(error));
         });
@@ -32,6 +32,45 @@ describe('authLogin', () => {
     });
 });
 
-describe('authLogout', () => {
-
-})
+describe('adsLod',() => {
+    const action = adsLoad();
+    const dispatch = jest.fn();
+    const api = { ads: { } };
+    const state = { ads: { } };
+    describe('When ads areLoaded', () => {
+        test('Should return without follow the flow action', async () => {
+            state.ads.areLoaded = true;
+            state.ads.data = [{id:1}];
+            const getState = jest.fn().mockReturnValue(state);
+            api.ads.getAds = jest.fn();
+            action(dispatch, getState, {api});
+            expect(dispatch).not.toHaveBeenCalled();
+            expect(api.ads.getAds).not.toHaveBeenCalled();
+        });
+    });
+    describe('When ads api resolves', () => {
+        test('Should follow the flow action', async () => {
+            state.ads.areLoaded = false;
+            state.ads.data = [{id:1}];
+            const getState = jest.fn().mockReturnValue(state);
+            api.ads.getAds = jest.fn().mockResolvedValue(state.ads.data);
+            await action(dispatch, getState, {api});
+            expect(dispatch).toHaveBeenNthCalledWith(1, adsLoadedRequest());
+            expect(api.ads.getAds).toHaveBeenCalledTimes(1);
+            expect(dispatch).toHaveBeenNthCalledWith(2, adsLoadedSucces(state.ads.data));
+        });
+    });
+    describe('When ads api rejects', () => {
+        test('Should follow the error flow', async () => {
+            const error = new Error('error');
+            state.ads.areLoaded = false;
+            state.ads.data = [];
+            const getState = jest.fn().mockReturnValue(state);
+            api.ads.getAds = jest.fn().mockRejectedValue(error);
+            const promise = action(dispatch, getState, {api});
+            await expect(promise).rejects.toThrow(error);
+            expect(dispatch).toHaveBeenNthCalledWith(1, adsLoadedRequest());
+            expect(dispatch).toHaveBeenNthCalledWith(2, adsLoadedFailure(error));
+        });
+    });
+});
